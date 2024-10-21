12b2aa1e6010801879ed90377fa20af74b09201e
Thomas Agnola <34454686+Thomas16093@users.noreply.github.com>
2024-10-21 13:22:42 +0200

    initial window for the app

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World !")
+import tkinter
+
+if __name__ == "__main__":
+    # tkinter creation
+    m = tkinter.Tk()
+    m.title("Jobs list")
+    window_exit = tkinter.Button(m, text="Exit", command=m.destroy)
+    window_exit.pack()
+
+
+    m.mainloop()
